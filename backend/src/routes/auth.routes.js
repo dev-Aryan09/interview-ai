a2d590d2e84a1a92b86bc243d00f51a534abc1fc
Aryan Saini <09aryan.lucky@gmail.com>
2026-04-09 13:13:37 +0530

    feat: create register user API route and controller

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const authControllers = require("../controllers/auth.controller");
+
+const authRouter = express.Router();
+
+/**
+ * @route POST /api/auth/register
+ * @description Register a new user
+ * @access Public
+ */
+authRouter.post("/register", authControllers.registerUserController);
+
+module.exports = authRouter;
