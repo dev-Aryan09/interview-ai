27151ae4953f8bddc9cdd8e9d12d4081000c07fc
Aryan Saini <09aryan.lucky@gmail.com>
2026-04-20 16:35:37 +0530

    feat: create context file for auth state management

---
+++ frontend/src/features/auth/auth.context.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from "react";
+
+// initialize a new context object
+export const AuthContext = createContext();
+
+// wrapper component that provides value to uts descendents
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  return (
+    // Make values available to any component inside {children} without prop drilling
+    <AuthContext.Provider value={{ user, setUser, loading, setLoading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+/*
+Note :- 
+
+-> In prop drilling, data must pass through every layer (or component), even from unused ones, in order to reach the exact layer.
+
+but,
+
+-> Context API lets us "skip" those middle layers by providing data globally to any component that needs it.
+*/
