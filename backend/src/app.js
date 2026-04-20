86e24b8b0fee38c4788c327973527dc67141928c
Aryan Saini <09aryan.lucky@gmail.com>
2026-04-21 00:12:06 +0530

    feat: install cors package

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.use("/api/auth", authRoutes);
 
