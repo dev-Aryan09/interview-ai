0c665c365c9554ce11387b4d617bd3db15621db4
Aryan Saini <09aryan.lucky@gmail.com>
2026-04-21 00:50:46 +0530

    feat: implement login page with two-way binding, loading state and navigation

--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,10 +1,25 @@
-import { Link } from "react-router";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router";
+import { useAuth } from "../hooks/useAuth";
 import "../auth.form.scss";
 
 const Login = () => {
-  const handleSubmit = (e) => {
+  const { loading, handleLogin } = useAuth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    await handleLogin({ email, password });
+    navigate("/")
   };
+
+  if (loading) {
+    return <main>Loading...</main>;
+  }
   return (
     <main>
       <div className="form-container">
@@ -15,18 +30,22 @@ const Login = () => {
             <label htmlFor="email">Email</label>
             <input
               type="email"
+              value={email}
               name="email"
               id="email"
               placeholder="Enter your email"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
             <input
               type="password"
+              value={password}
               name="password"
               id="password"
               placeholder="Enter your password"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <button className="btn primary-btn">Login</button>
