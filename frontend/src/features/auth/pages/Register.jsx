aeae2a911ed399544b4db64e570d4d7e83488f6d
Aryan Saini <09aryan.lucky@gmail.com>
2026-04-22 03:10:30 +0530

    feat: implement login page with two-way binding, loading state and navigation

--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router";
+import { useAuth } from "../hooks/useAuth";
 import "../auth.form.scss";
-import { Link } from "react-router";
 
 const Register = () => {
-  const handleSubmit = (e) => {
+  const { loading, handleRegister } = useAuth();
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    await handleRegister({ username, email, password });
+    navigate("/login");
   };
+
+  if (loading) {
+    return <main>Loading...</main>;
+  }
+
   return (
     <main>
       <div className="form-container">
@@ -15,27 +32,33 @@ const Register = () => {
             <label htmlFor="username">Username</label>
             <input
               type="text"
+              value={username}
               name="username"
               id="username"
               placeholder="Enter username"
+              onChange={(e) => setUsername(e.target.value)}
             />
           </div>
           <div className="input-group">
             <label htmlFor="email">Email</label>
             <input
               type="email"
+              value={email}
               name="email"
               id="email"
               placeholder="Enter email address"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
             <input
               type="password"
+              value={password}
               name="password"
               id="password"
               placeholder="Enter password"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <button className="btn primary-btn">Register</button>
