d2d2dc94a9f2e423b025729a0b3fbd2cd7e93dc0
Aryan Saini <09aryan.lucky@gmail.com>
2026-04-28 03:24:38 +0530

    refactor: update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
@@ -14,6 +13,12 @@ app.use(
   }),
 );
 
+/* require all the routes here */
+const authRoutes = require("./routes/auth.routes");
+const interviewRoutes = require("./routes/interview.routes");
+
+/* using all the routes here */
 app.use("/api/auth", authRoutes);
+app.use("/api/interview", interviewRoutes);
 
 module.exports = app;
